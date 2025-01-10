de61d6bcf837ddf699e9bbd9ab59a4e1f3003a06
jeb-767 <767.jeb.767@gmail.com>
2025-01-10 10:27:27 +0100

    Comit inicial

--- Split Wise Project/Form_Menu.Designer.cs
+++ Split Wise Project/Form_Menu.Designer.cs
@@ -1,14 +1,14 @@
 ﻿namespace Split_Wise_Project
 {
-    partial class Form1
+    partial class Form_Menu
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,90 +23,79 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            panel1 = new Panel();
-            But_Hist = new PictureBox();
             But_Groups = new PictureBox();
             But_Friends = new PictureBox();
-            panel2 = new Panel();
-            ((System.ComponentModel.ISupportInitialize)But_Hist).BeginInit();
+            But_Hist = new PictureBox();
+            panel1 = new Panel();
             ((System.ComponentModel.ISupportInitialize)But_Groups).BeginInit();
             ((System.ComponentModel.ISupportInitialize)But_Friends).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)But_Hist).BeginInit();
             SuspendLayout();
-            // 
-            // panel1
-            // 
-            panel1.Location = new Point(12, 12);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(315, 397);
-            panel1.TabIndex = 3;
-            // 
-            // But_Hist
-            // 
-            But_Hist.Image = Properties.Resources.But_Hist1;
-            But_Hist.Location = new Point(223, 415);
-            But_Hist.Name = "But_Hist";
-            But_Hist.Size = new Size(98, 23);
-            But_Hist.SizeMode = PictureBoxSizeMode.Zoom;
-            But_Hist.TabIndex = 6;
-            But_Hist.TabStop = false;
             // 
             // But_Groups
             // 
             But_Groups.Image = Properties.Resources.But_Groups;
-            But_Groups.Location = new Point(12, 415);
+            But_Groups.Location = new Point(12, 371);
             But_Groups.Name = "But_Groups";
-            But_Groups.Size = new Size(98, 23);
+            But_Groups.Size = new Size(110, 28);
             But_Groups.SizeMode = PictureBoxSizeMode.Zoom;
-            But_Groups.TabIndex = 7;
+            But_Groups.TabIndex = 0;
             But_Groups.TabStop = false;
             // 
             // But_Friends
             // 
             But_Friends.Image = Properties.Resources.But_Friends;
-            But_Friends.Location = new Point(119, 415);
+            But_Friends.Location = new Point(128, 371);
             But_Friends.Name = "But_Friends";
-            But_Friends.Size = new Size(98, 23);
+            But_Friends.Size = new Size(104, 28);
             But_Friends.SizeMode = PictureBoxSizeMode.Zoom;
-            But_Friends.TabIndex = 8;
+            But_Friends.TabIndex = 1;
             But_Friends.TabStop = false;
             // 
-            // panel2
+            // But_Hist
             // 
-            panel2.Location = new Point(12, 12);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(309, 397);
-            panel2.TabIndex = 9;
+            But_Hist.Image = Properties.Resources.But_Hist;
+            But_Hist.Location = new Point(238, 371);
+            But_Hist.Name = "But_Hist";
+            But_Hist.Size = new Size(110, 28);
+            But_Hist.SizeMode = PictureBoxSizeMode.Zoom;
+            But_Hist.TabIndex = 2;
+            But_Hist.TabStop = false;
             // 
-            // Form1
+            // panel1
+            // 
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(336, 353);
+            panel1.TabIndex = 3;
+            // 
+            // Form_Menu
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(340, 450);
-            Controls.Add(panel2);
+            ClientSize = new Size(360, 450);
+            Controls.Add(panel1);
+            Controls.Add(But_Hist);
             Controls.Add(But_Friends);
             Controls.Add(But_Groups);
-            Controls.Add(But_Hist);
-            Name = "Form1";
-            Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)But_Hist).EndInit();
+            Name = "Form_Menu";
+            Text = "Form_Menu";
             ((System.ComponentModel.ISupportInitialize)But_Groups).EndInit();
             ((System.ComponentModel.ISupportInitialize)But_Friends).EndInit();
+            ((System.ComponentModel.ISupportInitialize)But_Hist).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
-        private Button button3;
-        private Panel panel1;
-        private PictureBox pictureBox1;
-        private PictureBox But_Friends;
-        private Panel panel2;
-        private PictureBox But_Hist;
+
         private PictureBox But_Groups;
         private PictureBox But_Friends;
+        private PictureBox But_Hist;
+        private Panel panel1;
     }
 }
---
+++ Split Wise Project/Form_Menu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Split_Wise_Project
+{
+    public partial class Form_Menu : Form
+    {
+        public Form_Menu()
+        {
+            InitializeComponent();
+        }
+    }
+}
